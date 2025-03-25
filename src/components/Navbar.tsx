e6af488459f24515cb1ab201bb99f0aefcf02924
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-25 20:46:07 +0530

    refactor Navbar and header styles; implement responsive design and toggle functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,22 @@
 function Navbar() {
+
+    const toggleMenu = () => {
+        const nav = document.querySelector('.nav__container') as HTMLElement;
+        if (nav.style.display === 'none') {
+            nav.style.display = 'flex';
+        }
+        else {
+            nav.style.display = 'none';
+        }
+    }
+
+
     return (
         <nav className="nav body-2">
+            <div className="nav__logo">
+                <img src="src/assets/logo.png" alt="logo" />
+            </div>
             <div className="nav__container">
-                <div className="nav__logo">
-                    <img src="src/assets/logo.png" alt="logo" />
-                </div>
                 <ul className="nav__links">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">Service</a></li>
@@ -12,17 +24,20 @@ function Navbar() {
                     <li><a href="#contact">Product</a></li>
                     <li><a href="#">Testimonial</a></li>
                     <li><a href="#">FAQ</a></li>
+
                 </ul>
-                <div className="nav__auth-links">
-                    <a href="#login" className="login">Login</a>
-                    <a href="#signup" className="signup">Sign Up</a>
-                </div>
+                <ul className="nav__links">
+                    <li><a href="#login" className="login">Login</a></li>
+                    <li><a href="#signup" className="signup">Sign Up</a></li>
+                </ul>
             </div>
             <div className="nav__hamburger">
-                <i className="ri-menu-fill" />
+                <i className="ri-menu-fill" onClick={toggleMenu} />
             </div>
         </nav>
     )
 }
+
+
 
 export default Navbar;
