d344b1d30698a8d100260f660ea8ca8d8c162206
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-26 14:36:32 +0530

    add font family to buttons; update header styles for better alignment and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ function Header() {
             <div className="header__container">
                 <h1 className="header__title">Lessons and insights <span className="header__span">from 8 years</span></h1>
                 <p className="header__para">Where to grow your business as a photographer: site or social media?</p>
+                <a href="#" className="button">Register</a>
             </div>
             <div className="header__hero">
                 <img src="src/assets/hero-img.png" alt="hero-img" className="header__hero--img"/>
