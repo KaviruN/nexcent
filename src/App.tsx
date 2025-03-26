352ee7ed7bb00288fad5e7904c22f67fdf1585f3
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-26 11:17:39 +0530

    add Header component with styling; update App to include Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/style.css";
 import 'remixicon/fonts/remixicon.css'
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="containner">
       <Navbar />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__title">Lessons and insights <span className="header__span">from 8 years</span></h1>
+                <p className="header__para">Where to grow your business as a photographer: site or social media?</p>
+            </div>
+            <div className="header__hero">
+                <img src="src/assets/hero-img.png" alt="hero-img" className="header__hero--img"/>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
+// This component is a placeholder for the header of the application.
