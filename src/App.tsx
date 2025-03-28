26a7d05e92bb83541f3ad41646c18b2867606a99
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-28 12:01:46 +0530

    add Clients component to display client images; include corresponding styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./styles/style.css";
 import 'remixicon/fonts/remixicon.css'
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Clients from "./components/Clients";
 
 function App() {
   return (
     <div className="containner">
       <Navbar />
       <Header />
+      <Clients />
     </div>
   )
 }
---
+++ src/components/Clients.tsx
@@ -0,0 +1,26 @@
+function Clients() {
+  return (
+    <section className="client">
+      <div className="client__img-container">
+        <img src="src/assets/client-1.png" />
+      </div>
+      <div className="client__img-container">
+        <img src="src/assets/client-2.png" />
+      </div>
+      <div className="client__img-container">
+        <img src="src/assets/client-3.png" />
+      </div>
+      <div className="client__img-container">
+        <img src="src/assets/client-4.png" />
+      </div>
+      <div className="client__img-container">
+        <img src="src/assets/client-5.png" />
+      </div>
+      <div className="client__img-container">
+        <img src="src/assets/client-6.png" />
+      </div>
+    </section>
+  );
+}
+
+export default Clients;
