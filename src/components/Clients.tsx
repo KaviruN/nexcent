224596c1b189ec519e1167daaa5cb4cce0a14f15
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-28 21:05:17 +0530

    add Clients component with marquee effect for client images; update styles and typography

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,23 +1,34 @@
+import Marquee from "react-fast-marquee";
+
+
 function Clients() {
   return (
     <section className="client">
-      <div className="client__img-container">
-        <img src="src/assets/client-1.png" />
+      <div className="client__head">
+        <h2>Our Clients</h2>
+        <p>We have been working with some Fortune 500+ clients</p>
       </div>
-      <div className="client__img-container">
-        <img src="src/assets/client-2.png" />
-      </div>
-      <div className="client__img-container">
-        <img src="src/assets/client-3.png" />
-      </div>
-      <div className="client__img-container">
-        <img src="src/assets/client-4.png" />
-      </div>
-      <div className="client__img-container">
-        <img src="src/assets/client-5.png" />
-      </div>
-      <div className="client__img-container">
-        <img src="src/assets/client-6.png" />
+      <div className="client__images">
+        <Marquee speed={90} pauseOnHover={true} gradient={false}>
+          <div className="client__images--container">
+            <img src="src/assets/client-1.png" />
+          </div>
+          <div className="client__images--container">
+            <img src="src/assets/client-2.png" />
+          </div>
+          <div className="client__images--container">
+            <img src="src/assets/client-3.png" />
+          </div>
+          <div className="client__images--container">
+            <img src="src/assets/client-4.png" />
+          </div>
+          <div className="client__images--container">
+            <img src="src/assets/client-5.png" />
+          </div>
+          <div className="client__images--container">
+            <img src="src/assets/client-6.png" />
+          </div>
+        </Marquee>
       </div>
     </section>
   );
