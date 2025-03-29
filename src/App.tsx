b8f6e9d1ca1905e2fdad508ec9aa89e189c1a320
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-29 19:58:23 +0530

    add Wraper component and styles; remove unused image asset; update styles index

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Clients from "./components/Clients";
 import Cards from "./components/Cards";
+import Wraper from "./components/Wraper";
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
       <Header />
       <Clients />
       <Cards />
+      <Wraper
+        img="src/assets/wraper-img1.png"
+        title="The unseen of spending three years at Pixelgrade"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio."
+      />
     </div>
   )
 }
