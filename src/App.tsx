0f9d99712d3361981b2966e96264f6c38124119b
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-31 01:15:57 +0530

    fix: correct typo in title of Wraper component

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <Cards />
       <Wraper
         img={wraperImg1}
-        title="The unseen of spending three years at Pixelgrade"
+        title="The unseen of spending three years at Pixelkgrade"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio."
       />
       <Stats />
