e950fa19492b83c051c0897588e34aa063f25b11
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-30 19:01:38 +0530

    add About component with initial structure

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+function About(){
+    return (
+        <div className="about">
+            <div className="about__img">
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default About;
