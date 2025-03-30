87f4dd4669e4f134a338b1ec7e4f365728e74a18
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-30 23:21:00 +0530

    add Demo component with header and button, style enhancements for layout

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,7 +1,8 @@
 function Demo(){
     return (
         <section className="demo">
-
+            <h1>Pellentesque suscipit fringilla libero eu.</h1>
+            <a href="#" className="button">Get a Demo <i className="ri-arrow-right-long-fill"></i></a>
         </section>
     )
 
