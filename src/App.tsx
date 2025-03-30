1a01c27297dd4da09f0eda0c272173d6d31ed014
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-03-30 20:00:21 +0530

    add About component with styles and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Clients from "./components/Clients";
 import Cards from "./components/Cards";
 import Wraper from "./components/Wraper";
 import Stats from "./components/Stats";
+import About from "./components/About";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         title="How to design your site footer like we did"
         text="Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida."
       />
+      <About />
     </div>
   )
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,27 @@
-function About(){
+function About() {
     return (
         <section className="about">
             <div className="about__img">
                 <img src="src/assets/about-img.png" alt="" />
             </div>
+            <div className="about__container">
+                <div className="about__txt">
+                    <p>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
+                    <h4>Tim Smith</h4>
+                    <p>British Dragon Boat Racing Association</p>
+                </div>
+                <div className="about__customers">
+                    <div className="about__logos">
+                        <img src="src/assets/client-1.png" alt="" />
+                        <img src="src/assets/client-2.png" alt="" />
+                        <img src="src/assets/client-3.png" alt="" />
+                        <img src="src/assets/client-4.png" alt="" />
+                        <img src="src/assets/client-5.png" alt="" />
+                        <img src="src/assets/client-6.png" alt="" />
+                    </div>
+                </div>
+            </div>
+
         </section>
     );
 }
